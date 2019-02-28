e3583fa582dd49a754bcdcf085abc9443d83d885
pmagee <patricia.magee@ittdublin.ie>
2019-02-28 16:32:38 +0000

    Updates

--- Lab 9 Stock Management/app/models/shopping/Basket.java
+++ Lab 9 Stock Management/app/models/shopping/Basket.java
@@ -117,10 +117,7 @@ public class Basket extends Model {
                     // remove object from list
                     iter.remove();
                     break;
-                }   
-                System.out.println("Inside If");
-                
-                
+                }      
             }
        }
     }
